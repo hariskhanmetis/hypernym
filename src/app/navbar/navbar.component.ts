8dfcf2d23c259b8f30279b70a97442d05ebb54ce
hariskhanmetis <hariskhan@metispvt.com>
2025-04-24 11:44:33 +0500

    Navbar's notifications menu has been completed!

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,32 +6,43 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements AfterViewInit {
-    @ViewChild('modal') modal!: ElementRef;
-    
-    isModalOpen = false;
-    modalVisible = false; 
-  
-    openModal() {
-      if (!this.isModalOpen) {
-        this.isModalOpen = true;
-        setTimeout(() => {
-          this.modalVisible = true;
-        }, 1); 
-      } else {
-        this.modalVisible = false;
-        setTimeout(() => {
-          this.isModalOpen = false;
-        }, 500); 
-      }
-    }
-    
-    closeModal() {
-      this.isModalOpen = false;
-      this.modalVisible = false;
-    }
-  
-    ngAfterViewInit() {
-      this.modalVisible = false;
-    }
+  @ViewChild('modal') modal!: ElementRef;
 
+  isModalOpen = false;
+  modalVisible = false;
+  currentMenu: string | null = null; 
+ 
+  openModal(menuName: string) {
+    if (!this.isModalOpen) {
+      this.isModalOpen = true;
+      this.currentMenu = menuName; 
+      setTimeout(() => {
+        this.modalVisible = true; 
+      }, 1);
+    } else if (this.currentMenu === menuName) {
+      this.modalVisible = false;
+      setTimeout(() => {
+        this.isModalOpen = false;
+        this.currentMenu = null;
+      }, 500);
+    } else {
+      this.modalVisible = false;
+      setTimeout(() => {
+        this.currentMenu = menuName; 
+        this.modalVisible = true;
+      }, 500);
+    }
+  }
+
+  closeModal() {
+    this.modalVisible = false;
+    setTimeout(() => {
+      this.isModalOpen = false;
+      this.currentMenu = null; 
+    }, 500);
+  }
+
+  ngAfterViewInit() {
+    this.modalVisible = false;
+  }
 }
