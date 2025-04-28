ce66283e6a164042a6eca3fa6d071c8aaafb70d8
hariskhanmetis <hariskhan@metispvt.com>
2025-04-28 14:59:10 +0500

    Drivers page from fleetpoint has been completed!

--- src/app/drivers/drivers.component.ts
+++ src/app/drivers/drivers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-drivers',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./drivers.component.css']
 })
 export class DriversComponent {
+  constructor (private router: Router) {}
 
+  navigateToDashboard () {
+    this.router.navigate(['/dashboard/fleetpoint/dashboards/']);
+  }
+
+  navigateToDrivers() {
+    this.router.navigate(['/dashboard/fleetpoint/drivers']);
+  }
 }
