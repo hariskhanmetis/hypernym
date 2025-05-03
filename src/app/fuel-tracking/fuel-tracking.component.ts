3a975989660732e3d8b590a77b040edfd3ac0cdc
hariskhanmetis <hariskhan@metispvt.com>
2025-05-03 15:20:39 +0500

    Sidebar and Navbar's width & height respectively has been adjusted and Fuel Tracking page has been completed!

--- src/app/fuel-tracking/fuel-tracking.component.ts
+++ src/app/fuel-tracking/fuel-tracking.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fuel-tracking',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./fuel-tracking.component.css']
 })
 export class FuelTrackingComponent {
+constructor (private router: Router) {}
 
+  @ViewChild('modal') modal!: ElementRef;
+
+  isModalOpen = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  ngAfterViewInit() {
+    this.isModalOpen = false;
+  }
+  
+    navigateToDashboard () {
+      this.router.navigate(['/dashboard/fleetpoint/dashboards/']);
+    }
+  
+    navigateToFuelTracking() {
+      this.router.navigate(['/dashboard/fleetpoint/fuel-tracking/']);
+    }
 }
